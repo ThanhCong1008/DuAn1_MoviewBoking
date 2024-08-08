2de1541429b34d81050b2098ee7cf64984e820c2
ThanhCong1008 <lecong10082004gmail.com>
2024-08-08 17:30:25 +0700

    congcmt

---
+++ app/src/main/java/com/example/duan1_moviewwbooking/activity/ThongTinVeActivity.java
@@ -0,0 +1,26 @@
+package com.example.duan1_moviewwbooking.activity;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.example.duan1_moviewwbooking.R;
+
+public class ThongTinVeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_thong_tin_ve2);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+}
